0d5915c037d278dd9ec60f221df8d24c4d3b76f8
AdrianEstevarengo <132377900+AdrianEstevarengo@users.noreply.github.com>
2024-07-24 19:20:03 -0400

    criando modelo

---
+++ ConsumindoAPI/Models/Musica.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text.Json.Serialization;
+
+namespace ConsumindoAPI.Models;
+
+internal class Musica
+{
+    [JsonPropertyName("song")]
+    public string Nome { get; set; }
+    [JsonPropertyName("artist")]
+    public string Artista { get; set; }
+    [JsonPropertyName("genre")]
+    public string Genero { get; set; }
+    [JsonPropertyName("duration_ms")]
+    public int Duracao { get; set; }
+
+    public void ExibirMusicas()
+    {
+        Console.WriteLine($"Musica : {Nome}");
+        Console.WriteLine($"Artista : {Artista}");
+        Console.WriteLine($"Genero : {Genero}");
+        Console.WriteLine($"Duração : {Duracao}");
+    }
+}
--- ConsumindoAPI/Program.cs
+++ ConsumindoAPI/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿using ConsumindoAPI.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+using (HttpClient client = new HttpClient())
+{
+    try
+    {
+        string resposta = await client.GetStringAsync("https://guilhermeonrails.github.io/api-csharp-songs/songs.json")!;
+        var musicas = JsonSerializer.Deserialize < List<Musica>>(resposta);
+        musicas[69].ExibirMusicas();
+    }                 
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Temos um problema: {ex.Message}");
+    }
+}
+
